749d4b2b5ddc0ad2daae899db93103c520075660
Dan Shearmur <dan.shearmur@semantico.com>
2012-11-04 16:00:17 +0000

    Added localstorage plugin to backbone

--- client/app/config.js
+++ client/app/config.js
@@ -24,7 +24,11 @@ require.config({
     },
 
     // Backbone.LayoutManager depends on Backbone.
-    "plugins/backbone.layoutmanager": ["backbone"]
+    "plugins/backbone.layoutmanager": ["backbone"],
+
+    // Backbone.localstorage depends on Backbone.
+    "plugins/backbone-localstorage": ["backbone"]
+
   }
 
 });
